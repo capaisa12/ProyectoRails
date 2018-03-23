3c5b2e96279e90f789032321427fdfdf071ab73a
capaisa12 <capaisa12@gmail.com>
2018-03-22 20:36:29 -0500

    Commit de mailer

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+
+after_create :send_email
+
+private
+
+def send_email
+UserMailer.welcome_email(self).deliver_now
+end
+
+end
